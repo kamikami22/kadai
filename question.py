103a32bdcadc2115bb4a584d29381b9d3b8e841c
kamikami22 <52307816+kamikami22@users.noreply.github.com>
2019-07-26 13:11:47 +0900

    Create question.py

---
+++ question.py
@@ -0,0 +1,9 @@
+#question for you
+
+print('What is your favorite color?')
+
+print('Where do you want to go in the summer vacation?')
+
+print('What did you do yesterday?')
+
+print('Thank you for your answer(^^)')
